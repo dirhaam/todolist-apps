cb73adb76b2b6ecb76ab2cab98d442097745f226
dirhaam <dirhamrozi@gmail.com>
2026-01-19 16:19:13 +0700

    chore: Update D1 database ID in Drizzle configuration and Wrangler settings.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     driver: "d1-http",
     dbCredentials: {
         accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-        databaseId: "757a32d1-5779-4f09-bcf3-b268013395d4",
+        databaseId: "b0883b7c-db60-4716-aabd-70fd6184720f",
         token: process.env.CLOUDFLARE_D1_TOKEN!,
     },
 });
